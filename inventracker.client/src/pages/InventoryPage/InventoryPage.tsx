991551f6fea65c3ee0d1a44e0e494dc03ec13421
zlplayer <konzoboron@gmail.com>
2026-06-24 23:06:56 +0200

    nagłówki na stronie inwentory i uzytkowników

--- inventracker.client/src/pages/InventoryPage/InventoryPage.tsx
+++ inventracker.client/src/pages/InventoryPage/InventoryPage.tsx
@@ -1,3 +1,44 @@
+import styles from './InventoryPage.module.sass'
+import { Plus, Users, Clock, CircleCheckBig, Wrench, Box } from 'lucide-react'
+
 export default function InventoryPage() {
-  return <h1>Inwentarz - jesteś zalogowany!</h1>
+  return (
+    <div className="container">
+      <div className={styles.header}> 
+        <div className={styles.headerIcon}>
+           <Box />
+          <div className={styles.title}>
+            <span className={styles.titleText}>Inwentarz</span>
+            <span className={styles.descriptionTitle}>Pelna lista przedmiotow w systemie</span>
+          </div>
+        </div>
+        <button className={styles.addInventoryItemButton}>
+          <Plus size={16} />
+          Dodaj przedmiot
+        </button>
+      </div>
+      <div className={styles.stats}>
+        <span className={styles.statItem}>
+          <CircleCheckBig size={16} color="green"/>
+          Dostepne: <strong>8</strong>
+        </span>
+        <span className={styles.statItem}>
+          <Clock size={16} color="blue"/>
+          W uzyciu: <strong>12</strong>
+        </span>
+        <span className={styles.statItem}>
+          <Wrench size={16} color="orange"/>
+          Konserwacja: <strong>12</strong>
+        </span>
+        <span className={styles.statItem}>
+          Ilosc przedmiotów: <strong>12</strong>
+        </span>
+      </div>
+
+      <div className={styles.inventory}>
+
+      </div>
+      
+    </div>
+  )
 }
--- inventracker.client/src/pages/UsersPage/UsersPage.tsx
+++ inventracker.client/src/pages/UsersPage/UsersPage.tsx
@@ -1,3 +1,45 @@
+import styles from './UserPage.module.sass'
+import { Plus, Users, UserX, UserCheck, Shield } from 'lucide-react'
+
 export default function UsersPage() {
-  return <h1>Użytkownicy - jesteś zalogowany!</h1>
+
+   return (
+    <div className="container">
+      <div className={styles.header}> 
+        <div className={styles.headerIcon}>
+           <Users />
+          <div className={styles.title}>
+            <span className={styles.titleText}>Uzytkownicy</span>
+            <span className={styles.descriptionTitle}>Zarzadzaj uzytkownikami i uprawnieniami w systemie</span>
+          </div>
+        </div>
+        <button className={styles.addUserButton}>
+          <Plus size={16} />
+          Dodaj uzytkownika
+        </button>
+      </div>
+      <div className={styles.stats}>
+        <span className={styles.statItem}>
+          <UserCheck size={16} color="green"/>
+          Aktywni: <strong>8</strong>
+        </span>
+        <span className={styles.statItem}>
+          <UserX size={16}/>
+          Nieaktywni: <strong>12</strong>
+        </span>
+        <span className={styles.statItem}>
+          <Shield size={16} color="blue"/>
+          Administratorzy: <strong>12</strong>
+        </span>
+        <span className={styles.statItem}>
+          Ilosc użytkowników: <strong>12</strong>
+        </span>
+      </div>
+
+      <div className={styles.users}>
+
+      </div>
+      
+    </div>
+  )
 }
